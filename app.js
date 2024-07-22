e8434d220c913973915d6089726268b9fc05a0a2
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-07-22 20:54:01 +0530

    public added

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express=require("express")
-
 const session = require("express-session")
 const app=express()
 
 const port=3000
+const path = require('path');
 const connectDB=require('./config/db')
-connectDB()
 const userRouter=require('./route/userRouter')
 const adminRouter=require('./route/adminRoute')
 const authRouter = require('./route/authRoute')
 app.set('view engine', 'ejs')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 const nocache = require("nocache");
 // const flash = require('express-flash');
 const flash = require('connect-flash');
@@ -28,18 +26,19 @@ app.use(flash());
 app.use(nocache())
 
 // app.use((req, res, next) => {
-//     // Apply cache control headers for all routes
-//     res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-//     res.set('Pragma', 'no-cache');
-//     res.set('Expires', '0');
-//     res.set('Surrogate-Control', 'no-store');
-  
-//     // Continue to the next middleware or route handler
-//     next();
-//   });
-
-app.use(session({
-    secret: 'your-secret-key',
+    //     // Apply cache control headers for all routes
+    //     res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    //     res.set('Pragma', 'no-cache');
+    //     res.set('Expires', '0');
+    //     res.set('Surrogate-Control', 'no-store');
+    
+    //     // Continue to the next middleware or route handler
+    //     next();
+    //   });
+    
+    connectDB()
+    app.use(session({
+        secret: 'your-secret-key',
     resave: false,
     saveUninitialized: false
 }));
