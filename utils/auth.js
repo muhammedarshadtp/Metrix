1d3386064d6c1e153e2152e2934396b9dbc290b2
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-10-09 12:40:11 +0530

    change the googleauthitication

--- utils/auth.js
+++ utils/auth.js
@@ -9,7 +9,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/auth/google/callback",
+    callbackURL: "https://metrixs.shop/auth/auth/google/callback",
+    // callbackURL: "http://localhost:3000/auth/auth/google/callback"
     passReqToCallback :true
   },
   function(request,accessToken, refreshToken, profile, done) {
